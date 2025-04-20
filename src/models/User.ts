61d66c410e2db7a5f657d3cb3654ac1a61311b71
Folareen <sakawahab03@gmail.com>
2025-04-20 17:55:17 +0100

    fixed db sync issue

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,7 +5,7 @@ const User = sequelizeInstance.define('User', {
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        unique: 'email',
         validate: {
             isEmail: true,
         }
