5d168820bcd3a04047ee47fd6e3c377a012e8ffe
Folareen <sakawahab03@gmail.com>
2025-05-04 19:14:01 +0100

    fix

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ app.use('/api/v1/', userRouter)
 app.use(notFound)
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 (async () => {
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,8 @@
 import { Dialect, Sequelize } from "sequelize";
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT, DB_PORT } = process.env;
 
-if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_DIALECT) {
+if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_DIALECT || !DB_PORT) {
     throw new Error('Missing database configuration in environment variables');
 }
 
@@ -10,6 +10,7 @@ if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_DIALECT) {
 const sequelizeInstance = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: DB_DIALECT as Dialect,
+    port: DB_PORT as unknown as number
 });
 
 export default sequelizeInstance
