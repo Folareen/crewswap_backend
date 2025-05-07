447cde0a894b980fd3ba7e3b0e72125a2c1babd0
Folareen <sakawahab03@gmail.com>
2025-05-07 22:21:12 +0100

    done with get preferred schedules

---
+++ src/controllers/schedule/getPreferredSchedules.ts
@@ -0,0 +1,60 @@
+import { Response } from "express";
+import { Op } from "sequelize";
+import Preference from "../../models/Preference";
+import Schedule from "../../models/Schedule";
+import { AuthenticatedReq } from "../../types/authenticatedReq";
+
+export default async (req: AuthenticatedReq, res: Response) => {
+    try {
+        const userId = req?.user?.id
+
+        const userPreference = await Preference.findOne({ where: { userId } })
+
+        if (!userPreference) {
+            res.status(400).json({ message: 'Update your preferences first' })
+            return
+        }
+
+        const userPreferenceObj = userPreference.dataValues
+
+        Object.entries(userPreferenceObj).forEach(([key, value]) => {
+            if (value === null) {
+                delete userPreferenceObj[key]
+            }
+        })
+
+        delete userPreferenceObj.userId
+        delete userPreferenceObj.createdAt
+        delete userPreferenceObj.updatedAt
+        delete userPreferenceObj.id
+
+        if (userPreferenceObj.datesOff.length == 0) {
+            delete userPreferenceObj.datesOff
+        }
+
+        const matchingPreferences = await Preference.findAll({
+            where: {
+                ...userPreferenceObj,
+                userId: { [Op.ne]: userId }
+            }
+        })
+
+        const preferredSchedules = await Promise.all(matchingPreferences.map(async (preference) => {
+            return Schedule.findOne({ where: { userId: preference.dataValues.userId } })
+        }))
+
+        if (!preferredSchedules) {
+            res.status(400).json({ message: 'No preferred schedules found' })
+            return
+        }
+
+        res.status(200).json({ schedules: preferredSchedules })
+    }
+
+    catch (error) {
+        console.log(error, 'errrorrr')
+
+        res.status(500).json({ message: 'Internal server error' })
+        return
+    }
+}
--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import syncSchedule from "../controllers/schedule/syncSchedule";
 import authenticate from "../middlewares/authenticate";
+import getPreferredSchedules from "../controllers/schedule/getPreferredSchedules";
 
 const router = Router()
 
 router.route('/schedule').post(authenticate, syncSchedule)
+router.route('/preferred-schedules').get(authenticate, getPreferredSchedules)
 
 export default router
